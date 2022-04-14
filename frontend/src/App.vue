b8025acbeb28ed0591e08a1973900a606da4eb8c
souamandaleal <a.amandaleal@hotmail.com>
2022-04-14 13:07:01 -0300

    Adicionando o vue 2

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,29 @@
-<script setup>
-import Home from './components/Home.vue'
-</script>
-
 <template>
-  <header>
-
-    <div>
-      <Home />
-    </div>
-  </header>
-
-  <main>
-  </main>
+  <div id="app">
+    <nav></nav>
+    <router-view />
+  </div>
 </template>
 
-<style></style>
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
